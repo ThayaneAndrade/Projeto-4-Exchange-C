17e80a6ea6e9d4eaa45f24b11040563516ef5316
KaiqueMDEV <161653647+KaiqueMDEV@users.noreply.github.com>
2025-04-10 17:22:23 +0000

    Criada a função de depósito + atualização das informações dos usuários no arquivo após saída do menu principal, dessa forma os valores são mantidos sempre que executar o código novamente + comentários adicionais no código e pequenos ajustes

--- Codes/funcoes.h
+++ Codes/funcoes.h
@@ -1,26 +1,38 @@
 #define funcoes_h
-
+ 
 typedef struct usuario { //struct que define as características de cada usuário.
     char cpf[12];
     char nome[50];
     int senha;
     float saldo;
-
+    float btc;
+    float eth;
+    float xrp;
+ 
 }usuario;
-
+ 
 typedef struct lista{ //struct que será usada para organizar os usuários por "índices" através do qtd, além de armazenar os usuarios definidos em ponteiros.
     usuario *vetor[100];
     int qtd;
 }lista;
-
+ 
 void registrar(lista *Lista);
-
+ 
 int login(lista *Lista);
-
-void menuprincipal(lista *Lista, int *logado, int indice_logado);
-
+ 
+void menuprincipal(lista *Lista, int *indice_logado);
+ 
+int solicita_senha(lista Lista, int indice_logado);
+ 
 int inserir_usuario(lista *Lista, usuario *user);
-
+ 
 void debug_imprimir_lista(lista *l);
-
+ 
 void carregar_usuarios(lista *Lista);
+ 
+void saldo(lista *Lista, int indice_logado);
+
+void deposito(lista *Lista, int indice_logado);
+
+void arquivar_usuarios(lista *Lista, int indice_logado);
+ 
--- Codes/main.c
+++ Codes/main.c
@@ -2,18 +2,15 @@
 #include <string.h>
 #include <stdlib.h>
 #include "funcoes.h"
-
+ 
 int main() {
-    int indice_logado = -1;
-    int senhalogada;
-    int logado = 0;
-    lista *Lista = malloc(sizeof(lista));
-
-    
-    printf("\n\n-----Bem vindo(a) a CryptoSpy 2.0-----\n");
+    int indice_logado = -1; //índica dinamicamente qual usuário está conectado, baseado no seu índice no vetor de usuarios (EX: 1 = Kaique, 2 = Thayane, 3 = ...)
+    lista *Lista = malloc(sizeof(lista)); //declara a lista de usuarios
+    carregar_usuarios(Lista); //carrega os usuarios do arquivo para o vetor de ponteiros
+ 
+    printf("\n\n-----Bem vindo(a) a CryptoSpy 2.0-----\n"); //título
     int opcao;
-    while (1) {
-        if (!logado){
+    while (indice_logado == -1) { //enquanto o usuário não estiver logado, entrará na tela de login.
             printf("\n1. Registrar\n");
             printf("2. Login\n");
             printf("3. Sair\n");
@@ -21,24 +18,20 @@ int main() {
             printf("\nEntrada: ");
             scanf("%d", &opcao);
             if(opcao == 1){
-                logado = 0;
                 registrar(Lista);
             }else if(opcao == 2){
-                if ((indice_logado = login(Lista)) != -1) {
-                logado = 1;
-                menuprincipal(Lista, &logado, indice_logado);
+                if ((indice_logado = login(Lista)) != -1) { //tenta fazer login, se bem sucedido, armazena o índice do usuário conectado, se não, índice -1 (ninguém)
+                menuprincipal(Lista, &indice_logado);
                 }
             }else if(opcao == 3){
-                logado = 0;
                 printf("\nDesligando...\n");
                 break;
             }else if(opcao == 4){
-                logado = 0;
                 debug_imprimir_lista(Lista);
             }else{
                 printf("Opção inválida. Tente novamente.\n");
             }
-    } 
 }
 return 0;    
 }
+ 
